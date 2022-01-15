c80faf963c4d9d810e4fdf8ec20ab37ba8b24b2d
Abhilash Singh (Hacker Charles) <84007524+Hacker-Charles@users.noreply.github.com>
2022-01-15 18:52:25 +0530

    Add files via upload

---
+++ monitor-mode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+\033[1m \e[32m
+echo -e "Enter interface"
+read interface
+ifconfig $interface down
+iwconfig $interface mode monitor
+ifconfig $interface up
+iwconfig $interface
+echo -e ""
+echo -e "\033[31m Monitor Mode Enabled Successfully \033[31m"
+echo -e ""
+echo -e "\033[94m Don't Forget to Subscribe to Hacker Charles on YouTube ! \033[94m"
